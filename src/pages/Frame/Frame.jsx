8922aa7fd13a400d0544de2b53dced161ca85297
YeahOut <131945340+YeahOut@users.noreply.github.com>
2023-12-20 09:20:12 +0900

    add: 눈사람 등록

---
+++ src/pages/Frame/Frame.jsx
@@ -0,0 +1,60 @@
+import {
+  DivRoot,
+  GoBackIcon,
+  Div1,
+  Icon,
+  Icon1,
+  WrapperGroup314,
+  WrapperGroup314Child,
+  Child,
+  ButtonsCtaSecondary,
+  ButtonsCtaSecondary1,
+  RectangleParent,
+  GroupChild,
+  Div2,
+  RectangleGroup,
+  FrameChild, 
+  FrameItem,
+  B,
+  B1,
+  Sns,
+  B2,
+  Div3,
+  Wrapper1,
+  Icon2
+}from "./styled"
+
+export const Frame = () => {
+  return (
+    <DivRoot>
+      <GoBackIcon alt="" src="/go-back@2x.png" />
+      <Div1>눈사람 등록</Div1>
+      <Icon alt="" src="/snow.png" />
+      <Icon1 alt="" src="/snow.png" />
+      <WrapperGroup314>
+        <WrapperGroup314Child alt="" src="/group-314@2x.png" />
+      </WrapperGroup314>
+      <Child />
+      <ButtonsCtaSecondary>
+        <ButtonsCtaSecondary1 />
+      </ButtonsCtaSecondary>
+      <RectangleParent>
+        <GroupChild />
+        <Div2>스포티한 눈썰매형</Div2>
+        <RectangleGroup>
+          <FrameChild />
+          <FrameItem />
+        </RectangleGroup>
+        <B>이름 : 땡땡이</B>
+        <B1>한줄 소개 : 담요를 덮었어요</B1>
+        <Sns>SNS : 공개</Sns>
+        <B2>성별 : 남성</B2>
+      </RectangleParent>
+      <Div3>눈사람 등록하기</Div3>
+      <Wrapper1>
+        <Icon2 alt="" src="/-1@2x.png" />
+      </Wrapper1>
+    </DivRoot>
+  );
+};
+
---
+++ src/pages/Frame/styled.js
@@ -0,0 +1,225 @@
+import styled from "styled-components";
+
+export const GoBackIcon = styled.img`
+  position: absolute;
+  top: 1rem;
+  left: 0.44rem;
+  width: 2.69rem;
+  height: 2.69rem;
+  object-fit: cover;
+  mix-blend-mode: hard-light;
+`;
+export const Div1 = styled.div`
+  position: absolute;
+  top: 5.63rem;
+  left: 0.06rem;
+  display: inline-block;
+  width: 24.31rem;
+  height: 1.63rem;
+`;
+export const Icon = styled.img`
+  position: absolute;
+  top: 5.81rem;
+  left: 7.13rem;
+  width: 1.5rem;
+  height: 1.44rem;
+  object-fit: cover;
+`;
+export const Icon1 = styled.img`
+  position: absolute;
+  top: 5.81rem;
+  left: 15.81rem;
+  width: 1.5rem;
+  height: 1.44rem;
+  object-fit: cover;
+`;
+export const WrapperGroup314Child = styled.img`
+  height: 100%;
+  width: 100%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+  object-fit: contain;
+  position: absolute;
+  left: 0rem;
+  top: 0rem;
+  transform: scale(1.187);
+`;
+export const WrapperGroup314 = styled.div`
+  position: absolute;
+  height: 5.67%;
+  width: 29.05%;
+  top: 1%;
+  right: 35.82%;
+  bottom: 91.05%;
+  left: 35.13%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const Child = styled.div`
+  position: absolute;
+  top: 4.78rem;
+  left: -0.03rem;
+  border-top: 1px solid var(--color-black);
+  box-sizing: border-box;
+  width: 100%;
+  height: 0.06rem;
+`;
+export const ButtonsCtaSecondary1 = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0%;
+  right: 0%;
+  bottom: 0%;
+  left: 0%;
+  border-radius: var(--br-6xl);
+`;
+export const ButtonsCtaSecondary = styled.button`
+  cursor: pointer;
+  border: none;
+  padding: 0;
+  background-color: transparent;
+  position: absolute;
+  height: 5.97%;
+  width: 84.87%;
+  top: 60.12%;
+  right: 7.69%;
+  bottom: 9.91%;
+  left: 7.44%;
+  border-radius: 5px;
+  background: linear-gradient(213.37deg, #fff, #bebebe);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  backdrop-filter: blur(4px);
+  mix-blend-mode: normal;
+`;
+export const GroupChild = styled.div`
+  position: absolute;
+  top: 0rem;
+  left: 0rem;
+  border-radius: 40px;
+  background-color: #2c4fa4;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border: 3px dashed var(--color);
+  box-sizing: border-box;
+  width: 20.38rem;
+  height: 27.31rem;
+`;
+export const Div2 = styled.div`
+  position: absolute;
+  top: 2.13rem;
+  left: calc(50% - 113px);
+  font-size: 1.81rem;
+  letter-spacing: -0.41px;
+  line-height: 1.56rem;
+  font-weight: 800;
+  font-family: var(--font-tmoney-roundwind);
+  color: var(--color);
+  text-align: center;
+`;
+export const FrameChild = styled.div`
+  position: absolute;
+  top: calc(50% - 60px);
+  left: calc(50% - 138px);
+  border-radius: var(--br-3xs);
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 17.63rem;
+  height: 10.25rem;
+`;
+export const FrameItem = styled.div`
+  position: absolute;
+  top: calc(50% - 60px);
+  left: calc(50% - 143px);
+  border-radius: var(--br-3xs);
+  background-color: #cddcff;
+  width: 17.63rem;
+  height: 9.88rem;
+`;
+export const RectangleGroup = styled.div`
+  position: absolute;
+  top: 15.75rem;
+  left: 4.06rem;
+  width: 12.5rem;
+  height: 6.25rem;
+`;
+export const B = styled.b`
+  position: absolute;
+  top: 16.06rem;
+  left: 2.75rem;
+  letter-spacing: -0.41px;
+  line-height: 1.88rem;
+`;
+export const B1 = styled.b`
+  position: absolute;
+  top: 18rem;
+  left: 2.75rem;
+  letter-spacing: -0.41px;
+  line-height: 1.88rem;
+`;
+export const Sns = styled.b`
+  position: absolute;
+  top: 21.81rem;
+  left: 2.75rem;
+  letter-spacing: -0.41px;
+  line-height: 1.88rem;
+`;
+export const B2 = styled.b`
+  position: absolute;
+  top: 19.88rem;
+  left: 2.75rem;
+  letter-spacing: -0.41px;
+  line-height: 1.88rem;
+  display: inline-block;
+  width: 4.75rem;
+  height: 1.13rem;
+`;
+export const RectangleParent = styled.div`
+  position: absolute;
+  top: 8.38rem;
+  left: 3rem;
+  width: 20.38rem;
+  height: 27.31rem;
+  text-align: left;
+  font-size: var(--font-size-mini);
+  color: var(--color-black);
+  font-family: var(--font-noto-sans-kr);
+`;
+export const Div3 = styled.div`
+  position: absolute;
+  top: 62%;
+  left: calc(50% - 97px);
+  display: inline-block;
+  width: 12.13rem;
+  height: 0.88rem;
+`;
+export const Icon2 = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  position: absolute;
+  left: 0rem;
+  top: 0.25rem;
+  transform: scale(1.058);
+`;
+export const Wrapper1 = styled.div`
+  position: absolute;
+  top: 12.06rem;
+  left: calc(50% - 69px);
+  width: 8.63rem;
+  height: 11.69rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const DivRoot = styled.div`
+  position: relative;
+  background-color: #d1e5ed;
+  width: 100%;
+  height: 70rem;
+  overflow: hidden;
+  text-align: center;
+  font-size: var(--font-size-5xl);
+  color: var(--color-gray-100);
+  font-family: var(--font-kotra-hope);
+`;
